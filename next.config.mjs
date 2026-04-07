39f29bf16ba38cbc02e50d73822bb3cdef427fdf
Rithishabinav-pixel <rithish.pixel@gmail.com>
2026-04-07 15:22:29 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: true,
+  },
+
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"], 
+  display: 'swap',
 });
 
 export const metadata = {
